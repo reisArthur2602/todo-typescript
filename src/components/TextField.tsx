1ecb6a6a9888a9595d0820cd6c25d81ed1ad979b
arthur <arthurdesouzareis222@gmail.com>
2024-04-01 15:14:14 -0300

    feat: criar textfield

---
+++ src/components/TextField.tsx
@@ -0,0 +1,11 @@
+
+export const TextField = () => {
+  return (
+    <input
+    type="text"
+    placeholder="Criar nova Tarefa"
+    required
+    className="w-full outline-none bg-slate-200 p-3 rounded-xl w-full"
+  />
+  )
+}
