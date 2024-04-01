afaf311ca78b67a70cbd454244b9c4bfe528921a
arthur <arthurdesouzareis222@gmail.com>
2024-04-01 14:55:51 -0300

    feat: commit inicial

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import { Todo } from './components/Todo';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>
+);
