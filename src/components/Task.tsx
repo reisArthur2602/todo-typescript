e077671f1981bcbe33c664735c3f97275b472718
arthur <arthurdesouzareis222@gmail.com>
2024-04-01 15:51:46 -0300

    style: adicionar animacao a task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,12 @@
 import { Square, SquareCheck } from 'lucide-react';
 export const Task = () => {
   return (
-    <li className="w-full bg-white p-3 rounded-lg">
+    <li className="w-full bg-white p-3 rounded-lg hover:animate-bounce">
       <div className="flex gap-4 items-center">
         <button>
           <Square />
         </button>
-        <p>Estudar Bando de dados</p>
+        <p className="flex-1 line-clamp-2">Estudar Bando de dados</p>
       </div>
     </li>
   );
